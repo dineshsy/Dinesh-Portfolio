437256d83ea762d5a3d61bdd20430b9eb77d960c
dineshsy <s.ydinesh1177@gmail.com>
2020-08-02 17:41:15 +0530

    Change: About me content

--- src/Container/About/About.js
+++ src/Container/About/About.js
@@ -1,18 +1,17 @@
 import React from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSnowman } from "@fortawesome/free-solid-svg-icons";
-
 import classes from "./About.module.css";
 import Heading from "../../Components/Heading/Heading";
 import AboutContent from "../../Components/AboutContent/AboutContent";
-
+import PaperScroll from "../../Components/PaperScroll/PaperScroll";
 const about = () => {
     return (
         <section className={classes.About} id="about">
             <Heading text={"About"}>
                 <FontAwesomeIcon icon={faSnowman} className={classes.Icon} />
             </Heading>
+            <PaperScroll />
             <AboutContent />
         </section>
     );
