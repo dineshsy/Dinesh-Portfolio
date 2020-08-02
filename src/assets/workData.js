db50dc25c897c81d4229186c11cd6788d7675b46
dineshsy <s.ydinesh1177@gmail.com>
2020-08-02 17:28:42 +0530

    Add: Work Data

---
+++ src/assets/workData.js
@@ -0,0 +1,34 @@
+const WorkData = [
+    {
+        name: "Prevail",
+        img: `${process.env.PUBLIC_URL}/Cards/card-4.jpg`,
+        url: "https://github.com/dineshsy/Prevail",
+    },
+    {
+        name: "Foodshala",
+        img: `${process.env.PUBLIC_URL}/Cards/card-5.jpg`,
+        url: "https://github.com/dineshsy/FoodShala",
+    },
+    {
+        name: "Do it",
+        img: `${process.env.PUBLIC_URL}/Cards/card-1.jpg`,
+        url: "https://github.com/dineshsy/Do-it",
+    },
+    {
+        name: "D-Diagnose",
+        img: `${process.env.PUBLIC_URL}/Cards/card-2.jpg`,
+        url: "https://github.com/dineshsy/D-Diagnose",
+    },
+    {
+        name: "Notodo",
+        img: `${process.env.PUBLIC_URL}/Cards/card-3.jpg`,
+        url: "https://github.com/dineshsy/notodo_app",
+    },
+    {
+        name: "AI in Insurance Claims",
+        img: `${process.env.PUBLIC_URL}/Cards/card-6.jpg`,
+        url: "https://github.com/dineshsy/AI-in-Insurance-Claims",
+    },
+];
+
+export default WorkData;
