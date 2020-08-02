47360a926c72a996086aebdbd74883636bf22b10
dineshsy <s.ydinesh1177@gmail.com>
2020-08-02 17:35:15 +0530

    Update: Landing Header

--- src/Components/Home/HeadingPrimary/HeadingPrimaryMain/HeadinPrimaryMain.js
+++ src/Components/Home/HeadingPrimary/HeadingPrimaryMain/HeadinPrimaryMain.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-import classes from './HeadingPrimaryMain.module.css'
+import classes from "./HeadingPrimaryMain.module.css";
 const headingPrimayMain = () => {
     return (
         <div className={classes.title}>
             <span className={classes.block}></span>
-            <h1 className={classes.HeadingPrimaryMain}>Hola, I'm Dinesh!</h1>
+            <h1 className={classes.HeadingPrimaryMain}>
+                Hola, I'm{" "}
+                <span
+                    style={{
+                        fontSize: "7rem",
+                        fontWeight: 600,
+                        letterSpacing: "4px",
+                    }}
+                >
+                    Dinesh!
+                </span>
+            </h1>
         </div>
     );
-}
+};
 
 export default headingPrimayMain;
