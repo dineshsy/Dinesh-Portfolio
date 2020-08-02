6553cac8e1b6bdb52a7844b4216b0ca722bbc3f3
dineshsy <s.ydinesh1177@gmail.com>
2020-08-02 17:42:40 +0530

    Update: Work section layout

--- src/Container/Work/Work.js
+++ src/Container/Work/Work.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWrench } from "@fortawesome/free-solid-svg-icons";
+import works from "../../assets/workData";
+import classes from "./Work.module.css";
+import Heading from "../../Components/Heading/Heading";
+import Cards from "../../Components/UI/Cards/Cards";
+import styled from "styled-components";
+import { schemeCategory10 } from "d3-scale-chromatic";
+const WorksImgWrapper = styled.div`
+    display: grid;
+    margin: 0 0rem;
+    grid-template-columns: repeat(auto-fit, minmax(28rem, 1fr));
+    @media (min-width: 600px) {
+        grid-template-columns: repeat(auto-fit, minmax(40rem, 1fr));
+        margin: 0 10rem;
+    }
+`;
 
-import classes from './Work.module.css'
-import Heading from '../../Components/Heading/Heading'
-import Cards from '../../Components/UI/Cards/Cards'
-
-const work = ()=> {
+const work = () => {
     return (
         <section className={classes.Work} id="work">
             <Heading text={"Work"}>
                 <FontAwesomeIcon icon={faWrench} className={classes.Icon} />
             </Heading>
-            <Cards />
+            <WorksImgWrapper>
+                {works.map(({ img, url }, i) => (
+                    <Cards img={img} url={url} color={schemeCategory10[i]} />
+                ))}
+            </WorksImgWrapper>
         </section>
     );
-}
+};
 
-export default work
+export default work;
